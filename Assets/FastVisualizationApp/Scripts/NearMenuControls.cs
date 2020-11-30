7d5374edb84f7f368b3a23b84618f377a6a97272
Srinjoy Majumdar <srmajumd@microsoft.com>
2020-11-30 08:32:50 -0800

    add MRTK and new scene with MRTK controls

---
+++ Assets/FastVisualizationApp/Scripts/NearMenuControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.SceneUnderstanding.Samples.Unity;
+
+public class NearMenuControls : MonoBehaviour
+{
+    [SerializeField]
+    private FastSceneUnderstandingManager sceneUnderstandingManager;
+    [SerializeField]
+    private Interactable AutoRefreshToggle;
+    [SerializeField]
+    private Interactable InferRegionsToggle;
+
+    private void Start()
+    {
+        AutoRefreshToggle.IsToggled = sceneUnderstandingManager.AutoRefresh;
+        InferRegionsToggle.IsToggled = sceneUnderstandingManager.RequestInferredRegions;
+    }
+
+    public void UpdateSU()
+    {
+        sceneUnderstandingManager.StartDisplay();
+    }
+
+    public void ToggleAutoRefresh(bool enabled)
+    {
+        sceneUnderstandingManager.AutoRefresh = enabled;
+        if (!sceneUnderstandingManager.AutoRefresh)
+        {
+            sceneUnderstandingManager.TimeElapsedSinceLastAutoRefresh = sceneUnderstandingManager.AutoRefreshIntervalInSeconds;
+        }
+    }
+
+    public void UseMeshRenderingMode()
+    {
+        sceneUnderstandingManager.SceneObjectRequestMode = Microsoft.MixedReality.SceneUnderstanding.Samples.Unity.RenderMode.Mesh;
+    }
+
+    public void UseQuadRenderingMode()
+    {
+        sceneUnderstandingManager.SceneObjectRequestMode = Microsoft.MixedReality.SceneUnderstanding.Samples.Unity.RenderMode.Quad;
+    }
+
+    public void ToggleInferRegions(bool enabled)
+    {
+        sceneUnderstandingManager.RequestInferredRegions = enabled;
+    }
+}
